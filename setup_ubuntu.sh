5a9b98c051cad6a41b27a36a7def7e056245f8e7
Saul Pwanson <code@saul.pw>
2022-09-14 18:52:25 -0700

    [apt] remove missing packages

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -5,8 +5,8 @@ sudo add-apt-repository multiverse
 sudo add-apt-repository restricted
 sudo apt update
 
-sudo apt install -y vim-gtk blueman arandr i3 kitty rxvt-unicode-256color xclip pavucontrol
-sudo apt install -y libdvdnav4 libdvdread4 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libdvd-pkg
+sudo apt install -y vim-gtk blueman arandr i3 kitty xclip pavucontrol
+sudo apt install -y libdvdnav4 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libdvd-pkg
 sudo apt install -y ubuntu-restricted-extras libavcodec-extra ffmpeg
 sudo apt install -y tmux tmate mosh zsh vim git python3 exuberant-ctags cryptmount
 
